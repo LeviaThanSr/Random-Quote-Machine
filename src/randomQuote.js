3ea0d4d5d0285cd5eea3191cadb6994e7d9c5f29
LeviaThanSr <omarsarour@gmail.com>
2021-02-06 08:18:11 +0100

    Update 2: added style to the app

--- src/randomQuote.js
+++ src/randomQuote.js
@@ -5,40 +5,62 @@ class GetQuote extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      quote: quoteArr[randomIndex].quote,
-      author: quoteArr[randomIndex].author,
+      quote: "",
+      author: "",
     };
     this.handleClick = this.handleClick.bind(this);
     this.getRandomIndex = this.getRandomIndex.bind(this);
   }
-
-  handleClick() {
-    const randomIndex = this.getRandomIndex();
+  componentDidMount() {
     this.setState({
-      quote: quoteArr[randomIndex].quote,
-      author: quoteArr[randomIndex].author,
+      quote: quoteArr[this.getRandomIndex()].quote,
+      author: quoteArr[this.getRandomIndex()].author,
     });
   }
   getRandomIndex() {
     let quoteArrLength = quoteArr.length;
     return Math.floor(Math.random() * quoteArrLength);
   }
+
+  handleClick() {
+    this.setState({
+      quote: quoteArr[this.getRandomIndex()].quote,
+      author: quoteArr[this.getRandomIndex()].author,
+    });
+  }
   render() {
+    const twitterLink = `https://twitter.com/intent/tweet?text=${this.state.quote}%0D%0A%0D%0A-${this.state.author}`;
     return (
-      <div id="quote-box">
-        <div id="text">{this.state.quote}</div>
-        <div id="author">{this.state.author}</div>
-        <button id="new-quote" onClick={this.handleClick}>
-          New Quote
-        </button>
-        <a
-          href="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="
-          rel="noreferrer"
-          target="_blank"
-          id="tweet-quote"
-        >
-          Tweet Quote
-        </a>
+      <div className="card">
+        <div className="card-header">Random Quote Machine</div>
+        <div className="card-body" id="quote-box">
+          <h4 className="card-title" id="text">
+            {this.state.quote}
+          </h4>
+          <p className="card-text" id="author">
+            {this.state.author}
+          </p>
+          <div className="d-flex">
+            <button
+              id="new-quote"
+              type="button"
+              className="btn btn-success btn-block"
+              onClick={this.handleClick}
+            >
+              New Quote
+            </button>
+            <a
+              href={twitterLink}
+              rel="noreferrer"
+              target="_blank"
+              id="tweet-quote"
+              className="btn btn-primary btn-block"
+              role="button"
+            >
+              Tweet Quote
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
